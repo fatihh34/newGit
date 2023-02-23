5d5dbf9b383741b0f6ae81a6e09aaa013ef2cd2f
Omer Buyukergene <buyukergene@gotmail.com>
2023-02-24 00:05:30 +0300

    24.02.2023

---
+++ src/main/java/OmerDeneme.java
@@ -0,0 +1,9 @@
+public class OmerDeneme {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i = 0; i < 5; i++) {
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+}
