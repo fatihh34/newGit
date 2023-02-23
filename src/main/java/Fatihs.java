f670ffdb9472b4367a5fb7397834663bfcd80fcb
Fatih I <117350581+fatihh34@users.noreply.github.com>
2023-02-23 17:53:54 +0300

    v1

---
+++ src/main/java/Fatihs.java
@@ -0,0 +1,22 @@
+public class Fatihs {
+
+
+    public class FatihPractise {
+        public static void main(String[] args) {
+            //  Deneme deneme =new deneme();
+            deneme();
+
+        }
+
+        public static void deneme(){
+            System.out.println("hello world");
+        }
+        public void deneme2(){
+            System.out.println("goodbye world");
+        }
+        public void denem3(){
+            System.out.println("hi");
+        }
+    }
+
+}
